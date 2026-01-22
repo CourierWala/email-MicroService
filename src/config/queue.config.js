2c004cad250b1527887de547bf716bed938d41d7
Prashantkumar-Dudhmal <prashantkumardudhmal@gmail.com>
2026-01-22 15:26:14 +0530

    Local Deployable Email Service DONE!

--- src/config/queue.config.js
+++ src/config/queue.config.js
@@ -2,7 +2,7 @@ const { Queue } = require("bullmq");
 const IORedis = require("ioredis");
 
 const connection = new IORedis({
-  host: "127.0.0.1",
+  host: process.env.REDIS_HOST || "localhost",
   port: 6379,
   maxRetriesPerRequest: null,
 });
