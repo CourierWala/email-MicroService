951325da317c827dfe42ef88b1ad5743aba343c8
Prashantkumar-Dudhmal <prashantkumardudhmal@gmail.com>
2026-01-27 01:08:45 +0530

    Now Accepts Call From CorierWala Web

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-require('dotenv').config();
-const app = require('./src/app');
+require("dotenv").config();
+const app = require("./src/app");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
+// app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Email service running on port ${PORT}`);
--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 
-const emailRoutes = require('./routes/email.routes');
+const emailRoutes = require("./routes/email.routes");
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
-app.get('/health', (req, res) => {
-  res.send('Email Service is running');
+app.get("/health", (req, res) => {
+  res.send("Email Service is running");
 });
 
-app.use('/api/email', emailRoutes);
+app.use("/api/email", emailRoutes);
 
 module.exports = app;
